6470968fe6ba2a324ab5c73f033f3e7009a4a1e7
LeandroMarquex <43867278+LeandroMarquex@users.noreply.github.com>
2021-01-15 16:02:59 -0300

    Adicionei arquivos via upload
    Adicionei arquivos via upload

---
+++ pos-java-jdbc/src/main/java/conexaojdbc/SingleConnection.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package conexaojdbc;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author LEANDRO MARQUES
+ *
+ */
+public class SingleConnection {
+	private static String url = "jdbc:postgresql://localhost:5432/posjava";
+	private static String password = "123";
+	private static String user = "postgres";
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		try {
+			if (connection == null) {
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(url, user, password);
+				connection.setAutoCommit(false);
+				System.err.println("Efetuado conexão com sucesso");
+				
+			}
+			
+		} catch (Exception e) {
+			
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+}
---
+++ pos-java-jdbc/src/test/java/testeclasses/TesteBancoJdbc.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package testeclasses;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+
+import conexaojdbc.SingleConnection;
+import dao.UserPosDao;
+import model.Userposjava;
+
+/**
+ * @author JAVA - LEANDRO MARQUES
+ *
+ */
+public class TesteBancoJdbc {
+	
+	@Test
+	public void initBanco() {
+	//	SingleConnection.getConnection();
+		UserPosDao userPosDAO = new UserPosDao();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(5L);
+		userposjava.setNome("Matues teste");
+		userposjava.setEmail("mateus@gmail");
+		
+		userPosDAO.salvar(userposjava);
+	}
+	
+	@Test
+	public void initListar() {
+		UserPosDao dao = new UserPosDao();
+		try {
+			List<Userposjava> list = dao.listar();
+			
+			for (Userposjava userposjava : list) {
+				System.out.println(userposjava);
+				System.out.println("-----------------------------------");
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void initBuscar() {
+		
+		UserPosDao dao = new UserPosDao();
+		
+		try {
+			
+			Userposjava userposjava = dao.buscar(1L);
+			System.out.println(userposjava);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+}
